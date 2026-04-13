d835b68be2e3f8ac815e367dd980330b7dbd3a4e
Clansty <i@gao4.pw>
2026-04-13 21:13:15 +0800

    feat: add device mode selector with disconnect-on-switch
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- apps/webtest/src/App.tsx
+++ apps/webtest/src/App.tsx
@@ -3,15 +3,29 @@ import { RouterView } from 'vue-router';
 import { tryAutoReconnectTouch } from '@/devices/touchSerial';
 import { tryAutoReconnectLed } from '@/devices/ledSerial';
 import { tryAutoReconnectIO4 } from '@/devices/io4';
+import { tryAutoReconnectADX } from '@/devices/adxHid';
+import { tryAutoReconnectMML } from '@/devices/maimollerHid';
+import { deviceMode } from '@/devices/deviceMode';
 import { modalShowing, GlobalElementsContainer as UIGlobalElementsContainer } from '@munet/ui';
 import styles from './App.module.sass';
 
 export default defineComponent({
   setup() {
     onMounted(async () => {
-      await tryAutoReconnectTouch();
-      await tryAutoReconnectLed();
-      tryAutoReconnectIO4();
+      const mode = deviceMode.value;
+
+      if (mode !== 'maimoller') {
+        await tryAutoReconnectTouch();
+        await tryAutoReconnectLed();
+      }
+
+      if (mode === 'io4') {
+        await tryAutoReconnectIO4();
+      } else if (mode === 'adx') {
+        await tryAutoReconnectADX();
+      } else if (mode === 'maimoller') {
+        await tryAutoReconnectMML();
+      }
     });
 
     return () => <div
