16033888518b5bc006c2dfa4bcecd1af23cd42db
Clansty <i@gao4.pw>
2026-04-15 15:42:16 +0800

    feat: 全屏按钮

--- apps/webtest/src/App.tsx
+++ apps/webtest/src/App.tsx
@@ -1,18 +1,46 @@
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue';
 import { RouterView } from 'vue-router';
 import { tryAutoReconnectActiveDevice } from '@/devices/deviceMode';
 import { modalShowing, GlobalElementsContainer as UIGlobalElementsContainer } from '@munet/ui';
 import styles from './App.module.sass';
 
+const isFullscreen = ref(!!document.fullscreenElement);
+
+function toggleFullscreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    document.documentElement.requestFullscreen();
+  }
+}
+
+function onFullscreenChange() {
+  isFullscreen.value = !!document.fullscreenElement;
+}
+
 export default defineComponent({
   setup() {
-    onMounted(() => tryAutoReconnectActiveDevice());
+    onMounted(() => {
+      tryAutoReconnectActiveDevice();
+      document.addEventListener('fullscreenchange', onFullscreenChange);
+    });
+
+    onUnmounted(() => {
+      document.removeEventListener('fullscreenchange', onFullscreenChange);
+    });
 
     return () => <div
       class={[styles.contentRoot, modalShowing.value && styles.modalOpen]}
     >
       <UIGlobalElementsContainer />
       <RouterView />
+      <button
+        class="absolute top-2 right-2 rounded-full p-0 w-8 h-8 flex items-center justify-center"
+        onClick={toggleFullscreen}
+        title={isFullscreen.value ? '退出全屏' : '全屏'}
+      >
+        <div class={isFullscreen.value ? 'i-mingcute:fullscreen-exit-2-line' : 'i-mingcute:fullscreen-2-line'} />
+      </button>
     </div>;
   },
 });
